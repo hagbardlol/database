54e98154c89818c27b4808c1c550ea4a403042b0
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-05-07 12:40:35 -0800

    Add files via upload

---
+++ ScriptDatabase.lua
@@ -0,0 +1,343 @@
+return {    
+    Ahri = {
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Akali = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriAkali.lua"
+    },
+    Akshan = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Alistar = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinAlistar.lua"
+    },
+    Amumu = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua",   
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuAmumu.lua"
+    },
+    Annie = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuAnnie.lua"
+    },
+    Aphelios = {
+        "https://raw.githubusercontent.com/NoxusNA/BitsAndPiecesEverywhere/main/NAaphelios.lua"
+    },
+    Ashe = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyAshe.lua",   
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Azir = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriAzir.lua"
+    },
+    Blitzcrank = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuBlitzcrank.lua"
+    },
+    Brand = {
+    "https://github.com/Roburppey/BigSeries/raw/main/BigBrand.lua"
+    },
+    Caitlyn = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyCaitlyn.lua",   
+        "https://raw.githubusercontent.com/NoxusNA/BitsAndPiecesEverywhere/main/NACait.lua"
+    },
+    Camille = {
+        "https://robur.site/Rotatey/iSeries/raw/branch/main/iCamille.lua"
+    },
+    Cassiopeia = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinCassiopeia.lua"
+    },
+    Chogath = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigCho.lua"
+    },
+    Corki = {
+        "https://robur.site/Rotatey/iSeries/raw/branch/main/iCorki.lua",   
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Darius = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyDarius.lua"
+    },
+    DrMundo = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinDrMundo.lua"
+    },
+    Draven = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/DravenMechanics.lua"
+    },
+    Elise = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinElise.lua"
+    },
+    Evelynn = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinEvelynn.lua"
+    },
+    Ezreal = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyEzreal.lua",   
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Fiora = {
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua"
+    },
+    Garen = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigGaren.lua"
+    },
+    Gragas = {
+        "https://robur.site/Rotatey/iSeries/raw/branch/main/iGragas.lua"
+    },
+    Graves = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriGraves.lua"
+    },
+    Gwen = {
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua"
+    },
+    Hecarim = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Illaoi = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/IllaoiMechanics.lua"
+    },
+    Irelia = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinIrelia.lua",   
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Janna = {
+        "https://robur.site/gariban/GaribanSeries/raw/branch/master/GaribanJanna.lua"
+    },
+    Jax = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/JaxMechanics.lua"
+    },
+    Jayce = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinJayce.lua"
+    },
+    Jhin = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigJhin.lua"
+    },
+    Jinx = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyJinx.lua",   
+        "https://github.com/Roburppey/BigSeries/raw/main/BigJinx.lua"
+    },
+    Kaisa = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuKaiSa.lua"
+    },
+    Kalista = {
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua"
+    },
+    Karthus = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuKarthus.lua"
+    },
+    Kassadin = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuKassadin.lua"
+    },
+    Katarina = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriKatarina.lua"
+    },
+    Karma = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Kayle = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Kayn = {
+        "https://robur.site/Mistal/Public/raw/branch/master/MKayn.lua"
+    },
+    Kennen = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Kindred = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyKindred.lua"
+    },
+    Kled = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigKled.lua"
+    },
+    KogMaw = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuKogMaw.lua"
+    },
+    Leblanc = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/LeBlancMechanics.lua"
+    },
+    LeeSin = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriLeesin.lua"
+    },
+    Lissandra = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuLissandra.lua"
+    },
+    Leona = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyLeona.lua"
+    },
+    Lucian = {
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua"
+    },
+    Lulu = {
+        "https://robur.site/gariban/GaribanLulu/raw/branch/master/GaribanLulu.lua"
+    },
+    Lux = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigLux.lua"
+    },
+    Malphite = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuMalphite.lua"
+    },
+    Malzahar = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigMalzahar.lua"
+    },
+    MasterYi = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/MasterYiMechanics.lua"
+    },
+    MissFortune = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyMF.lua",   
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Mordekaiser = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigMordekaiser.lua"
+    },
+    Nami = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyNami.lua",   
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuNami.lua"
+    },
+    Nautilus = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Neeko = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuNeeko.lua"
+    },
+    Nocturne = {
+        "https://robur.site/Rotatey/iSeries/raw/branch/main/iNocturne.lua"
+    },
+    Olaf = {
+        "https://robur.site/Mistal/Public/raw/branch/master/MOlaf.lua"
+    },
+    Orianna = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinOrianna.lua",   
+        "https://robur.site/Maresh/Ponche/raw/branch/master/output/PoncheOrianna.lua"
+    },
+    Poppy = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinPoppy.lua"
+    },
+    Pyke = {
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Qiyana = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriQiyana.lua"
+    },
+    Quinn = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/QuinnMechanics.lua"
+    },
+    Renata = {
+        "https://robur.site/gariban/GaribanSeries/raw/branch/master/GaribanRenata.lua"
+    },
+    Ryze = {
+        "https://robur.site/wxx/KappaRobur/raw/branch/master/KappaRyze.lua"
+    },
+    Samira = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinSamira.lua",   
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Senna = {
+        "https://robur.site/wxx/KappaRobur/raw/branch/master/KappaSenna.lua"
+    },
+    Seraphine = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigSeraphine.lua"
+    },
+    Sett = {
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Shaco = {
+        "https://raw.githubusercontent.com/NoxusNA/BitsAndPiecesEverywhere/main/NAShaco.lua"
+    },
+    Shyvana = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuShyvana.lua"
+    },
+    Sion = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulySion.lua"
+    },
+    Sivir = {
+        "https://robur.site/Rotatey/iSeries/raw/branch/main/iSivir.lua"
+    },
+    Sona = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuSona.lua"
+    },
+    Soraka = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuSoraka.lua"
+    },
+    Swain = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Sylas = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/SylasMechanics.lua"
+    },
+    Syndra = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinSyndra.lua"
+    },
+    Talon = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyTalon.lua"
+    },
+    Teemo = {
+        "https://robur.site/Mistal/Public/raw/branch/master/MTeemo.lua"
+    },
+    Thresh = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuThresh.lua"
+    },
+    Tristana = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/TristanaMechanics.lua",   
+        "https://robur.site/hq.af/hq-scripts/raw/branch/master/PewPewTristana.lua"
+    },
+    TwistedFate = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigTwistedFate.lua"
+    },
+    Twitch = {
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Udyr = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyUdyr.lua"
+    },
+    Urgot = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/UrgotMechanics.lua"
+    },
+    Varus = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyVarus.lua"
+    },
+    Vayne = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyVayne.lua",   
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua"
+    },
+    Veigar = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigVeigar.lua"
+    },
+    Velkoz = {
+        "https://github.com/Roburppey/BigSeries/raw/main/BigVelkoz.lua"
+    },
+    Vex = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    Viktor = {
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua",
+        "https://robur.site/hq.af/hq-scripts/raw/branch/master/hqViktor.lua"
+    },
+    Volibear = {
+        "https://github.com/roburAURUM/robur-AuEdition/raw/main/AuVolibear.lua"
+    },
+    Xayah = {
+        "https://robur.site/MadJoee/Public/raw/branch/main/MadAIO.lua",   
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyXayah.lua"
+    },
+    Xerath = {
+        "https://robur.site/Thorn/Public/raw/branch/master/UnrulyXerath.lua",   
+        "https://github.com/samuelchow1997/robur-14AIO/raw/main/14AIO.lua"
+    },
+    XinZhao = {
+        "https://robur.site/RonaldinhoSoccer/CoolStuff/raw/branch/master/XinZhaoMechanics.lua"
+    },
+    Yasuo = {
+        "https://robur.site/Orietto/Public/raw/branch/master/OriYasuo.lua"
+    },
+    Yone = {
+        "https://robur.site/xSalice/Public/raw/branch/master/SallyAIO.lua"
+    },
+    Yorick = {
+        "https://robur.site/Mistal/Public/raw/branch/master/MYorick.lua"
+    },
+    Zeri = {
+        "https://robur.site/shulepin/robur/raw/branch/master/ShulepinZeri.lua"
+    },
+    Ziggs = {
+        "https://robur.site/ChewyMoon/Scripts/raw/branch/master/Iggy%20Ziggy.lua"
+    },
+    Utilities = {
+        "https://robur.site/Thorn/Public/raw/branch/master/PerfectWard.lua",
+        "https://github.com/Feez/Robur-DreamEvade/raw/master/DreamEvade.lua"
+    }
+}
