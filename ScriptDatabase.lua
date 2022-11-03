090dfed6d920dce8a9335d983ea26f2319d57e81
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-11-03 10:47:33 -0800

    Jinx Back

--- ScriptDatabase.lua
+++ ScriptDatabase.lua
@@ -128,6 +128,9 @@ return {
     Jhin = {
         "https://raw.githubusercontent.com/hagbardlol/Public/main/champions.lua"
     },
+    Jinx = {
+        "https://raw.githubusercontent.com/sigma-aio/release/main/SigmaAIO.lua"
+    },
     Kaisa = {
         "https://raw.githubusercontent.com/hagbardlol/Public/main/champions.lua"
     },
